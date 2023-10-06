87ec3a1c6207c01c986d1d0694a031d4c64f1bc1
Sebastian Duda <sebastian.duda@fim-rc.de>
2023-10-06 15:32:41 +0200

    Persist Databases

--- configure-db.sh
+++ configure-db.sh
@@ -5,37 +5,9 @@
 # and that system and user databases return "0" which means all databases are in an "online" state
 # https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-databases-transact-sql?view=sql-server-2017 
 
-sleep 20
+sleep 15
 
 # Run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i schema/DB_Schema.sql
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i schema/DB_Data.sql
-
-echo "Lorem ipsum, done SQL"
-
-exit 0
-
-
-DBSTATUS=1
-ERRCODE=1
-i=0
-
-while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt 60 ]] && [[ $ERRCODE -ne 0 ]]; do
-	i=$i+1
-	DBSTATUS=$(/opt/mssql-tools/bin/sqlcmd -h -1 -t 1 -U sa -P $SA_PASSWORD -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases")
-	ERRCODE=$?
-	sleep 1
-done
-
-if [ $DBSTATUS -ne 0 ] || [ $ERRCODE -ne 0 ]; then 
-	echo "SQL Server took more than 60 seconds to start up or one or more databases are not in an ONLINE state"
-	# exit 1
-fi
-
-# Run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i schema/DB_Schema.sql
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i schema/DB_Data.sql
-
-echo "Lorem ipsum, done SQL"
-
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i schema/db_setup.sql
+echo "DONE SQL"
 
