12f47fee3517c39001a34e5923b872d1208cb57e
Duda <sebastian.duda@fit.fraunhofer.de>
2023-10-22 19:56:04 +0200

    increase sql delay

--- configure-db.sh
+++ configure-db.sh
@@ -5,7 +5,7 @@
 # and that system and user databases return "0" which means all databases are in an "online" state
 # https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-databases-transact-sql?view=sql-server-2017 
 
-sleep 15
+sleep 30
 
 # Run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i schema/db_setup.sql
